46c2397383d20fec07f44a2c33c14ab638ab161b
Hlib Turuta <132139015+Arnaghad@users.noreply.github.com>
2024-01-22 16:55:47 +0100

    Add LessonNotification

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Telegram.BotAPI;
 using Telegram.BotAPI.GettingUpdates;
 using Newtonsoft.Json;
+using Nurebot;
 using NureBot.Classes;
 using NureBot.Handlers;
 using NureBot.Service;
@@ -26,40 +27,10 @@ if (!context.Teachers.Any())
     TeacherParser.Init();
 }
 
-Thread dbActualizer = new Thread(() =>
-{
-    while (true)
-    {
-        DateTime NowUTC = DateTime.UtcNow;
-        TimeZoneInfo kyivZone = TimeZoneInfo.FindSystemTimeZoneById("FLE Standard Time");
-        DateTime NowKyiv = TimeZoneInfo.ConvertTimeFromUtc(NowUTC, kyivZone);
-        if(NowKyiv.Hour == 0 && NowKyiv.Minute == 0 && NowKyiv.Second == 0)
-        {
-            var ToDelete = new List<Customer>();
-            foreach (var customer in context.Customers)
-            {
-                try
-                {
-                    Thread.Sleep(300);
-                    var message = client.SendMessage(customer.ChatId, "Ми перевіряємо чи не заблокували ви нас. Все нормально.",
-                        disableNotification: true);
-                    Thread.Sleep(1000);
-                    client.DeleteMessage(message.Chat.Id, message.MessageId);
-                }
-                catch (Exception e)
-                {
-                    ToDelete.Add(customer);
-                    Console.WriteLine(JsonConvert.SerializeObject(customer));
-                }
-            }
-
-            context.Customers.RemoveRange(ToDelete);
-            context.SaveChanges();
-        }
-    }
-});
-
-dbActualizer.Start();
+Thread thread1 = new Thread(new ThreadStart(dbService.dbActualizer(client)));
+Thread thread2 = new Thread(new ThreadStart(TimeService.LessonNotification(client)));
+thread1.Start();
+thread2.Start();
 
 while (true)
 {
--- Service/TimeService.cs
+++ Service/TimeService.cs
@@ -1,3 +1,9 @@
+using NureBot.Classes;
+using NureBot.Contexts;
+using NureBot.Parsers;
+using Telegram.BotAPI;
+using Telegram.BotAPI.AvailableMethods;
+
 namespace NureBot.Service;
 
 public class TimeService
@@ -7,5 +13,68 @@ public class TimeService
         DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         dateTime = dateTime.AddSeconds(unixTimeStamp);
         return dateTime;
+    }
+
+    public static void LessonNotification(BotClient bot)
+    {
+        while (true)
+        {
+            using (Context db = new Context())
+            {
+                Dictionary<string, List<Event>> Schedule = new Dictionary<string, List<Event>>();
+                List<string?> CistNames = new List<string?>();
+                DateTime NowUTC = DateTime.UtcNow;
+                TimeZoneInfo kyivZone = TimeZoneInfo.FindSystemTimeZoneById("FLE Standard Time");
+                DateTime NowKyiv = TimeZoneInfo.ConvertTimeFromUtc(NowUTC, kyivZone);
+                long NowKyivStamp = (long)NowKyiv.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+                if (NowKyiv.Hour == 6 && NowKyiv.Minute == 0 && NowKyiv.Second == 0)
+                {
+                    CistNames = db.Customers.Where(c => c.CistName != null)
+                        .Select(c => c.CistName).Distinct().ToList();
+                    foreach (var cistName in CistNames)
+                    {
+                        string Type;
+                        long? Cistid;
+                        if (cistName.Split().Length == 1)
+                        {
+                            Type = "group";
+                            Cistid = db.Groups.ToList().Find(x => x.Name == cistName).Id;
+                        }
+                        else
+                        {
+                            Type = "teacher";
+                            Cistid = db.Teachers.ToList().Find(x => x.ShortName == cistName).Id;
+                        }
+
+                        DateTime startDateTime = TimeZoneInfo.ConvertTime(DateTime.Now.Date, kyivZone);
+                        long startTime = (long)startDateTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+                        long endTime = startTime + 86400;
+                        List<Event> ScheduleEvent = EventParser.GetSchedule(Type, Cistid, startTime, endTime);
+                        if ((ScheduleEvent != null) || (ScheduleEvent.Count == 0))
+                        {
+                            ScheduleEvent = ScheduleEvent.OrderBy(c => c.StartTime).ToList();
+                            Schedule[cistName] = ScheduleEvent;
+                        }
+                    }
+                }
+
+                foreach (var cistName in CistNames)
+                {
+                    if ((Schedule[cistName][0].StartTime - NowKyivStamp) <= 300 && ((Schedule[cistName][0].StartTime - NowKyivStamp) > 0))
+                    {
+                        var ChatIds = db.Customers.Where(c => c.CistName == cistName)
+                            .Select(c => c.ChatId).ToList();
+                        foreach (var chatId in ChatIds)
+                        {
+                            bot.SendMessage(chatId,
+                                $"Нагадування! До пари з {Schedule[cistName][0].Subject.Title} ({Schedule[cistName][0].Type}) залишилося 5 хвилин.");
+                            Thread.Sleep(100);
+                        }
+                        Schedule[cistName].RemoveAt(0);
+                    }
+                }
+
+            }
+        }
     }
 }
